269be9339b4f4156ae9d7efc11801cb5a7b764a7
Raimundo Campos <raimundojr89@gmail.com>
2017-01-12 19:20:10 -0300

    Implementando botão de resetar jogo

--- js/main.js
+++ js/main.js
@@ -1,23 +1,51 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-$("#numero-palavras").text(numPalavras);
-
+/* Variáveis globais */
+var tempoInicial = $("#tempo-restante").text();
 var campo = $(".campo-digitacao");
-campo.on("input", function(){
-	var texto = $(".campo-digitacao").val();
-	var qtdPalavras = texto.split(/\S+/).length - 1;
-	$("#contador-palavras").text(qtdPalavras);
-	$("#contador-caracteres").text(texto.length);
+
+$(function(){
+	atualizaTamanhoFrase();
+	inicializaContadores();
+	inicializaCronometro();
+	$("#botao-reiniciar").click(reiniciaJogo);
+	$("#botao-reiniciar").attr("disabled", true);
 });
 
-var tempoRestante = $("#tempo-restante").text();
-campo.one("focus", function(){
-	var cronometroId = setInterval(function(){
-		tempoRestante--;
-		$("#tempo-restante").text(tempoRestante);
-		if(tempoRestante <= 0){
-			clearInterval(cronometroId);
-			$(".campo-digitacao").attr("disabled", true);
-		}
-	},1000);
-});
+function atualizaTamanhoFrase(){
+	var frase = $(".frase").text();
+	var numPalavras = frase.split(" ").length;
+	$("#numero-palavras").text(numPalavras);
+}
+
+function inicializaContadores(){
+	campo.on("input", function(){
+		var texto = $(".campo-digitacao").val();
+		var qtdPalavras = texto.split(/\S+/).length - 1;
+		$("#contador-palavras").text(qtdPalavras);
+		$("#contador-caracteres").text(texto.length);
+	});
+}
+
+function inicializaCronometro(){
+	var tempoRestante = tempoInicial
+	campo.one("focus", function(){
+		var cronometroId = setInterval(function(){
+			tempoRestante--;
+			$("#tempo-restante").text(tempoRestante);
+			if(tempoRestante <= 0){
+				clearInterval(cronometroId);
+				$(".campo-digitacao").attr("disabled", true);
+				$("#botao-reiniciar").attr("disabled", false);
+			}
+		},1000);
+	});
+}
+
+function reiniciaJogo(){
+	$(".campo-digitacao").attr("disabled", false);
+	$(".campo-digitacao").val("");
+	$("#contador-palavras").text("0");
+	$("#contador-caracteres").text("0");
+	$("#tempo-restante").text(tempoInicial);
+	inicializaCronometro();
+	$("#botao-reiniciar").attr("disabled", true);
+}
