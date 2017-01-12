3c3595c27045bd6c4dda39692d13311649d643c8
Raimundo Campos <raimundojr89@gmail.com>
2017-01-11 22:53:19 -0300

    Adicionando campo de texto e contando palavas/caracteres digitados

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
 $("#numero-palavras").text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+	var texto = $(".campo-digitacao").val();
+	var qtdPalavras = texto.split(/\S+/).length - 1;
+	$("#contador-palavras").text(qtdPalavras);
+	$("#contador-caracteres").text(texto.length);
+});
