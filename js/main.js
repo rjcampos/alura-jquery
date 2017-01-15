8d284f40cc9d8a4c80513e727b207fe47805c1af
Raimundo Campos <raimundojr89@gmail.com>
2017-01-15 15:11:32 -0300

    Implementando botao de remover linha do placar

--- js/main.js
+++ js/main.js
@@ -84,6 +84,7 @@ function inserePlacar(){
 	var numPalavras = $("#contador-palavras").text();
 
 	var linha = novaLinha(usuario, numPalavras);
+	linha.find(".botao-remover").click(removeLinha);
 	placar.append(linha);
 }
 
@@ -91,7 +92,21 @@ function novaLinha(usuario, numPalavras){
 	var linha = $("<tr>");
 	var nome = $("<td>").text(usuario);
 	var pontuacao = $("<td>").text(numPalavras);
+
+	var colunaRemove = $("<td>");
+	var link = $("<a>").attr("href", "#").addClass("botao-remover");
+	var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+	link.append(icone);
+	colunaRemove.append(link);
+
 	linha.append(nome);
 	linha.append(pontuacao);
+	linha.append(colunaRemove);
 	return linha;
+}
+
+function removeLinha(event){
+	event.preventDefault();
+	$(this).parent().parent().remove();
 }
