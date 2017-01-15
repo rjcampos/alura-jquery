462937a22b2bc3535648dab9f8300a3d3e564295
Raimundo Campos <raimundojr89@gmail.com>
2017-01-15 15:13:59 -0300

    Separando arquivo JS em varios para organização

--- js/main.js
+++ js/main.js
@@ -77,36 +77,3 @@ function reiniciaJogo(){
 	campo.removeClass("texto-errado");
 	campo.removeClass("texto-certo");
 }
-
-function inserePlacar(){
-	var placar = $(".placar").find("tbody");
-	var usuario = "Raimundo";
-	var numPalavras = $("#contador-palavras").text();
-
-	var linha = novaLinha(usuario, numPalavras);
-	linha.find(".botao-remover").click(removeLinha);
-	placar.append(linha);
-}
-
-function novaLinha(usuario, numPalavras){
-	var linha = $("<tr>");
-	var nome = $("<td>").text(usuario);
-	var pontuacao = $("<td>").text(numPalavras);
-
-	var colunaRemove = $("<td>");
-	var link = $("<a>").attr("href", "#").addClass("botao-remover");
-	var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-	link.append(icone);
-	colunaRemove.append(link);
-
-	linha.append(nome);
-	linha.append(pontuacao);
-	linha.append(colunaRemove);
-	return linha;
-}
-
-function removeLinha(event){
-	event.preventDefault();
-	$(this).parent().parent().remove();
-}
