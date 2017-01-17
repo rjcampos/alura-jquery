566676e23bba8c53c8bc9b2ba8faf1b358485dd1
Raimundo Campos <raimundojr89@gmail.com>
2017-01-17 19:40:10 -0300

    Implementando a escolha de frase pelo usuário

--- js/frase.js
+++ js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria(){
 	$("#spinner").toggle();
@@ -19,4 +20,27 @@ function trocaFraseAleatoria(data){
 	$(".frase").text(data[index].texto);
 	atualizaTamanhoFrase();
 	atualizaTempo(data[index].tempo);
+}
+
+function buscaFrase(){
+	$("#spinner").toggle();
+	var idFrase = $("#frase-id").val();
+	var objetoId = {id: idFrase};
+
+	$.get("http://localhost:3000/frases", objetoId, trocaFrase)
+	.fail(function(){
+		$("#erro").toggle();
+		setTimeout(function(){
+			$("#erro").toggle();
+		}, 2000);	
+	})
+	.always(function(){
+		$("#spinner").toggle();
+	});
+}
+
+function trocaFrase(data){
+	$(".frase").text(data.texto);
+	atualizaTamanhoFrase();
+	atualizaTempo(data.tempo);
 }
